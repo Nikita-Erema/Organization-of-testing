fbb244b83b0f04eeae1eb83e706e51e016879785
Nikita <k0bana@mail.ru>
2025-10-05 14:31:36 +0300

    commit

--- src/e2e/setup.js
+++ src/e2e/setup.js
@@ -2,19 +2,19 @@ const { spawn } = require('child_process');
 
 let serverProcess;
 
-export default async function globalSetup() {
+module.exports = async function globalSetup() {
   console.log('Запуск сервера...');
   serverProcess = spawn('npm', ['run', 'start'], {
     stdio: 'inherit',
     shell: true
   });
   
-  // Ждем подольше для запуска сервера
-  await new Promise(resolve => setTimeout(resolve, 8000));
+  console.log('Ждем запуска сервера...');
+  await new Promise(resolve => setTimeout(resolve, 10000));
   console.log('Сервер должен быть запущен');
 }
 
-export async function globalTeardown() {
+module.exports.globalTeardown = async function globalTeardown() {
   console.log('Остановка сервера...');
   if (serverProcess) {
     serverProcess.kill();
